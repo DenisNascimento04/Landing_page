92a6b376ca5010639cdb9a8b8c22d068d172301f
Denis <eledenis@hotmail.com>
2024-03-22 10:51:20 -0300

    1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { IoLogoInstagram, IoLogoTwitter, IoMdArrowRoundDown } from "react-icons/io";
 
-import { Card, CenterLarning, Container, Content, ContentCard, DescTitulo, DivHeader, DivInitContet, Header, Linha, NumeroFundo, Social, Texto, Titulo, TituloLarning } from  './styles';
+import { Card, Container, Content, DescTitulo, DivInitContet, Footer, Header, Linha } from  './styles';
 import HG from './assets/HG.png';
 import MG from './assets/MG.png';
 import VG from './assets/VG.png';
@@ -12,12 +12,16 @@ import Img3 from './assets/03.png';
 function App() {
   return (
     <Container>
-      <img src={HG} style={{  width: "100%", }} />
-      <img src={MG} style={{ marginTop: -565, width: "100%", height: 1422 }} />
-      <img src={VG} style={{ marginTop: -916, width: "100%", height: 926 }} />
-      <DivInitContet   />
-      <DivHeader>
-        <Header>
+      <div className="backs">
+        <div className='gradiente'/>
+        <img src={HG} alt='img-backs-1' />
+        <img src={MG} style={{ marginTop: -565, height: 1422 }} alt='img-backs-2' />
+        <img src={VG} style={{ marginTop: -916, height: 926 }} alt='img-backs-3' />
+      </div>
+      <DivInitContet />
+
+      <Header>
+        <nav>
           <p>MNTN</p>
           <div style={{ display: "flex", alignItems: "center" }}>
             <p style={{ marginRight: 40 }}>Equipament</p>
@@ -25,123 +29,125 @@ function App() {
             <p>Blog</p>
           </div>
           <p>Account</p>
-        </Header>
-        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", marginLeft: 80, marginRight: 80 }}>
-          <Social>
+        </nav>
+      </Header>
+
+      <Content> 
+        <div className='larning'>
+          <div className='social'>
             <p>Follow us</p>
             <IoLogoInstagram size={20} color="#ffff" />
             <IoLogoTwitter size={20} color="#ffff" />
-          </Social>
+          </div>
 
-          <CenterLarning>
+          <div className='center'>
             <div style={{ display: "flex", alignItems: "center" }}>
               <Linha />
               <DescTitulo style={{ color: "#FBD784" }}>A HIKING GUIDE</DescTitulo>
             </div>
 
-            <TituloLarning>
+            <h2>
               Be prepared for the Mountains and beyond!
-            </TituloLarning>
+            </h2>
 
             <div style={{ display: "flex", alignItems: "center" }}>
               <a href='#card1' style={{ fontSize: 18, textDecoration: 'none', color: "#FFFF" }}>scroll down</a>
               <IoMdArrowRoundDown size={18} color="#ffff" />
             </div>
-          </CenterLarning>
-          
-          <div></div>
+          </div>
         </div>
 
-      </DivHeader>
-      <Content>
         <Card id='card1'>
-          <NumeroFundo>
+          <div className='numBack'>
             <p>01</p>
-          </NumeroFundo>
-          <ContentCard style={{ marginRight: 114 }}>
+          </div>
+          <div className='contentCard'>
             <div style={{ display: "flex", alignItems: "center" }}>
               <Linha />
               <DescTitulo style={{ color: "#FBD784" }}>GET STARTED</DescTitulo>
             </div>
 
-            <Titulo>What level of  hiker are you?</Titulo>
-            <Texto style={{ width: 632 }}>
+            <h3>What level of  hiker are you?</h3>
+            <p style={{ width: 632 }}>
               Determining what level of hiker you are can be an important tool when planning future hikes.
               This hiking level guide will help you plan hikes according to different hike ratings set by various websites like 
               All Trails and Modern Hiker. What type of hiker are you – novice, moderate, advanced moderate, expert, or expert backpacker? 
-            </Texto>
+            </p>
 
             <div style={{ display: "flex", alignItems: "center" }}>
               <a href='#card2' style={{ fontSize: 18, textDecoration: 'none', color: "#FBD784", zIndex: 2 }}>read more</a>
               <IoMdArrowRoundDown size={18} color="#FBD784" />
             </div>
-          </ContentCard>
+          </div>
 
-          <img src={Img1} />
+          <img src={Img1} alt='img-card-1' />
         </Card>
 
-        <Card id='card2'>
-          <img src={Img2} />
-          <ContentCard style={{ marginLeft: 114 }}>
-            <NumeroFundo>
-              <p>02</p>
-            </NumeroFundo>
+        <Card id='card2' className='right'>
+          <img src={Img2} alt='img-card-2'/>
+          <div className='numBack right'>
+            <p>02</p>
+          </div>
+          <div className='contentCard'>
             <div style={{ display: "flex", alignItems: "center" }}>
               <Linha />
               <DescTitulo style={{ color: "#FBD784" }}>GET STARTED</DescTitulo>
             </div>
 
-            <Titulo>What level of  hiker are you?</Titulo>
-            <Texto style={{ width: 632 }}>
+            <h3>What level of  hiker are you?</h3>
+            <p style={{ width: 632 }}>
               Determining what level of hiker you are can be an important tool when planning future hikes.
               This hiking level guide will help you plan hikes according to different hike ratings set by various websites like 
               All Trails and Modern Hiker. What type of hiker are you – novice, moderate, advanced moderate, expert, or expert backpacker? 
-            </Texto>
+            </p>
 
             <div style={{ display: "flex", alignItems: "center" }}>
               <a href='#card3' style={{ fontSize: 18, textDecoration: 'none', color: "#FBD784", zIndex: 2 }}>read more</a>
               <IoMdArrowRoundDown size={18} color="#FBD784" />
             </div>
-          </ContentCard>
+          </div>
         </Card>
 
         <Card id='card3'>
-          <NumeroFundo>
+          <div className='numBack'>
             <p>03</p>
-          </NumeroFundo>
-          <ContentCard style={{ marginRight: 114 }}>
+          </div>
+          <div className='contentCard'>
             <div style={{ display: "flex", alignItems: "center" }}>
               <Linha />
               <DescTitulo style={{ color: "#FBD784" }}>WHERE YOU GO IS THE KEYS</DescTitulo>
             </div>
 
-            <Titulo>Understand Your Map & Timing</Titulo>
-            <Texto style={{ width: 632 }}>
+            <h3>Understand Your Map & Timing</h3>
+            <p style={{ width: 632 }}>
               To start, print out the hiking guide and map. If it’s raining, throw them in a Zip-Lock bag. 
               Read over the guide, study the map, and have a good idea of what to expect. I like to know what my next landmark is as I hike. 
               For example, I’ll read the guide and know that say, in a mile, I make a right turn at the junction..
-            </Texto>
+            </p>
 
             <div style={{ display: "flex", alignItems: "center" }}>
               <a href='#card1' style={{ fontSize: 18, textDecoration: 'none', color: "#FBD784", zIndex: 2 }}>read more</a>
               <IoMdArrowRoundDown size={18} color="#FBD784" />
             </div>
-          </ContentCard>
+          </div>
 
-          <img src={Img3} />
+          <img src={Img3} alt='img-card-3' />
         </Card>
 
-        <div style={{ display: "flex", justifyContent: 'space-evenly', padding: 4 }}>
-          <div style={{ display: 'flex', flexDirection: "column", marginRight: 700, justifyContent: "space-between", height: 272 }}>
+        <Footer >
+          <div className='desc' 
+          // style={{ marginRight: 700, height: 272 }}
+          >
             <div>
               <p>MNTN</p>
               <p style={{ width: 303 }}>Get out there & discover your next slope, mountain & destination!</p>
             </div>
             <p>Copyright 2019 MNTN, Inc. Terms & Privacy</p>
           </div>
-          <div style={{ display: "flex", gap: 24}}>
+
+          <div className='contatos'>
             <div style={{ display: "flex", flexDirection: "column"  }}>
-              <Titulo style={{ fontSize: 18, width: "auto", color: "#FBD784", lineHeight: 0, marginBottom: 12 }}>More on The Blog</Titulo>
+              <h3 style={{ fontSize: 18, width: "auto", color: "#FBD784", lineHeight: 0, marginBottom: 12 }}>More on The Blog</h3>
               <p>About MNTN</p>
               <p>Contributors & Writers</p>
               <p>Write For Us</p>
@@ -149,13 +155,13 @@ function App() {
               <p>Privacy Policy</p>
             </div>
             <div style={{ display: "flex", flexDirection: "column"  }}>
-              <Titulo style={{ fontSize: 18, width: "auto", color: "#FBD784", lineHeight: 0, marginBottom: 12 }}>More on MNTN</Titulo>
+              <h3 style={{ fontSize: 18, width: "auto", color: "#FBD784", lineHeight: 0, marginBottom: 12 }}>More on MNTN</h3>
               <p>The Team</p>
               <p>Jobs</p>
               <p>Press</p>
             </div>
           </div>
-        </div>
+        </Footer>
       </Content>
     </Container>
   );
--- src/styles.ts
+++ src/styles.ts
@@ -3,18 +3,49 @@ import styled from 'styled-components';
 export const Container = styled.div `
   background-color: #0B1D26;
   align-items: center;
+  position: relative;
+  /* padding: 0 20rem; */
   p{
     color: #FFFFFF;
   }
+  .backs{
+    position: absolute;
+    img{
+      z-index: 1;
+      object-fit: cover;
+      width: 100%;
+    }
+    .gradiente{
+      position: absolute;
+      background: linear-gradient(330.24deg, rgba(11, 29, 38, 0) 31.06%, #0B1D26 108.93%);
+      z-index: 2;
+      width: 100%;
+      height: 100%;
+    }
+  }
 `
-export const DivHeader = styled.div `
+export const Header = styled.header`
   position: absolute;
   width: 100%;
-  height: 1200px;
+  /* height: 1200px; */
   left: 0px;
   top: 0px;
-
-  background: linear-gradient(330.24deg, rgba(11, 29, 38, 0) 31.06%, #0B1D26 108.93%);
+  z-index: 10;
+  /* background: linear-gradient(330.24deg, rgba(11, 29, 38, 0) 31.06%, #0B1D26 108.93%); */
+  nav{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 32px 8rem;
+    p{
+      color: #FFFFFF;
+      font-family: 'Roboto';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 18px;
+      line-height: 22px;
+    }
+  }
 `
 export const DivInitContet = styled.div `
   position: absolute;
@@ -25,70 +56,10 @@ export const DivInitContet = styled.div `
 
   background: linear-gradient(180deg, rgba(11, 29, 38, 0) 0%, #0B1D26 61.38%);
 `
-
-export const Header = styled.div `
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin: 64px 80px;
-  p{
-    color: #FFFFFF;
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 18px;
-    line-height: 22px;
-  }
-`
 export const Linha = styled.div `
   background-color: #FBD784;
   width: 72px;
   height: 2px;
-`
-export const CenterLarning = styled.div`
-  /* background-color: green; */
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 0px;
-  gap: 24px;
-`
-export const TituloLarning = styled.p`
-  margin: 0px;
-  font-size: 88px;
-  width: 950px;
-  font-family: 'Playfair Display';
-  font-weight: 700;
-  /* padding: 0; */
-`
-export const Social = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0px;
-  margin-left: -24px;
-  gap: 24px;
-  /* background-color: red; */
-  p{
-    color: #FFFFFF;
-    transform: rotate(-90deg);
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 18px;
-    line-height: 22px;
-  }
-`
-
-export const Titulo = styled.p`
-  width: 632px;
-  /* background-color: red; */
-  font-family: 'Playfair Display';
-  font-weight: 700;
-  font-style: normal;
-  font-size: 64px;
-  margin: 0px;
-  line-height: 77px;
 `
 
 export const DescTitulo = styled.p`
@@ -97,59 +68,169 @@ export const DescTitulo = styled.p`
   letter-spacing: 6px;
 `
 
-
-export const Texto = styled.p`
-  width: 632px;
-  /* background-color: green; */
-  font-family: 'Roboto';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 18px;
-  line-height: 32px;
-`
-
 export const Content = styled.div`
-  margin-top: -650px;
+  /* margin-top: -650px; */
   padding-bottom: 120px;
   display: flex;
   flex-direction: column;
   align-items: center;
   /* width: 100%; */
   /* padding: 0 229px; */
+  padding-top: 24rem;
   justify-content: center;
+  z-index: 3;
   /* background-color: red; */
   gap: 200px;
+  .larning{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 3;
+    width: 100%;
+    margin: 0 8rem;
+    margin-bottom: 48rem;
+    position: relative;
+    .social{
+      display: flex;
+      position: absolute;
+      left: 8rem;
+      flex-direction: column;
+      align-items: center;
+      padding: 0px;
+      margin-left: -24px;
+      gap: 24px;
+      /* background-color: red; */
+      p{
+        color: #FFFFFF;
+        transform: rotate(-90deg);
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 18px;
+        line-height: 22px;
+      }
+    }
+    .center{
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      padding: 0px;
+      gap: 24px;
+      h2{
+        margin: 0px;
+        font-size: 8.8rem;
+        width: 1000px;
+        font-family: 'Playfair Display';
+        font-weight: 700;
+        color: #f2f2f2;
+      }
+    }
+  }
+  @media screen and (max-width: 1440px){
+    .larning{
+      .center{
+        h2{
+          font-size: 6.4rem;
+          width: 700px;
+        }
+      }
+    }
+  }
 `
 
 export const Card = styled.div`
   display: flex; 
-  align-items: center; 
+  align-items: center;
+  justify-content: space-between;
   z-index: 999;
+  /* background-color: red; */
+  width: 1200px;
+  height: 700px;
+  gap: 6rem;
+  /* width: 100%; */
+  &.right{
+  }
+  img{
+    width: 500px;
+    height: 100%;
+    object-fit: cover;
+  }
+  .numBack{
+    position: absolute;
+    /* top: 0px; */
+    &.right{
+      left: 47%;
+    }
+    p{
+      font-family: 'Roboto';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 240px;
+      line-height: 240px;
+      color: #FFFFFF;
+      opacity: 0.1;
+    }
+  }
+  .contentCard{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 0px;
+    gap: 16px;
+    h3{
+      width: 100%;
+      /* background-color: red; */
+      font-family: 'Playfair Display';
+      font-weight: 700;
+      font-style: normal;
+      font-size: 6.4rem;
+      margin: 0px;
+      line-height: 77px;
+      color: #f2f2f2;
+    }
+    p{
+      width: 100%;
+      /* background-color: green; */
+      font-family: 'Roboto';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 1.8rem;
+      line-height: 32px;
+    }
+  }
+  @media screen and (max-width: 1440px){
+    height: 600px;
+    .contentCard{
+      h3{
+        font-size: 4.8rem;
+      }
+      p{
+        font-size: 1.6rem;
+      }
+    }
+  }
 `
 
-
-export const ContentCard = styled.div`
+export const Footer = styled.footer`
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 0px;
-  gap: 16px;
-`
-
-
-export const NumeroFundo = styled.div`
-  position: absolute;
-  /* top: 0px; */
-  p{
-    font-family: 'Roboto';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 240px;
-    line-height: 240px;
-  
-  
-    color: #FFFFFF;
-  
-    opacity: 0.1;
+  justify-content: space-evenly;
+  /* background-color: green; */
+  width: 100%;
+  margin-top: 10rem;
+  .desc{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    /* background-color: red; */
+    p{
+      font-size: 1.4rem;
+    }
+  }
+  .contatos{
+    display: flex;
+    gap: 3.2rem;
+    p{
+      font-size: 1.6rem;
+    }
   }
 `
